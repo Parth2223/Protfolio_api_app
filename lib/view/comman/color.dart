6847ce4fea3e97e0ba67f22aaf2487ce3af646c8
Parth2223 <parthpadsala2020@gmail.com>
2023-12-15 09:58:37 +0530

    add color

---
+++ lib/view/comman/color.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+Color MainColor = Color(0xff123456);
+Color facbookColor = Color(0xff1959a9);
+Color black = Color(0xFF000000);
+Color white = Color(0xFFFFFFFF);
+Color appbarcolor = Color(0xE4E9EFFF);
+const Color demoWhite = Colors.white;
